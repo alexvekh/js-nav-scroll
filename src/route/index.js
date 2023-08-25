3da584c6c3927eb2bbfdc3b1f34c428bc04c9ef5
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-08-24 21:11:52 -0400

    html, css

--- src/route/index.js
+++ src/route/index.js
@@ -5,9 +5,11 @@ const router = express.Router()
 // Підключіть файли роутів
 const test = require('./test')
 // Підключіть інші файли роутів, якщо є
+const nav = require('./nav')
 
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
+router.use('/nav', nav)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
